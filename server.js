32dbef973483794d17d18e894d8cb63bf410ab28
Tom Bremer <tom@tbremer.com>
2015-04-12 21:56:26 -0500

    Add static file route, move css, add favicon

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
+'use strict';
+
 var http = require('http'),
     fs = require('fs'),
     Handlebars = require('handlebars'),
     checkRoutes = require('./lib/check-routes'),
     home = require('./lib/Routes/home'),
     nexturls = require('./lib/Routes/nexturls'),
+    static_files = require('./lib/static-files'),
     server = http.createServer,
     port = process.env.PORT || 1111,
     routes = {
       '/': home,
+      '/assets/([\\W\\w]+)?': {
+        content_type: '*',
+        action: static_files
+      },
       '/nexturls': {
         content_type: 'application/json',
         action: nexturls
@@ -38,11 +45,11 @@ server(function (req, res) {
     return res.end('404');
   }
 
-  if (valid_route.content_type !== 'application/json') {
+  if (valid_route.content_type === 'text/html') {
     res.write(templates.head);
   }
 
-  valid_route.action({res: res, query: query}, function (err, data) {
+  valid_route.action({res: res, query: query, url: url}, function (err, data) {
     data = data || templates.end;
 
     if (err) {
@@ -50,10 +57,10 @@ server(function (req, res) {
     }
 
     if (data.constructor === Object && valid_route.content_type === 'application/json') {
-      return res.end(JSON.stringify(data));
+     res.end(JSON.stringify(data));
     }
 
-    return res.end(data);
+    res.end(data);
   });
 
 }).listen(port, function () {
